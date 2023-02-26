3cb016b71ef54ef36fbdc92b2ec207bba71247c4
nilayarya <84241885+nilayarya@users.noreply.github.com>
2023-02-26 23:25:25 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.get('/', (req,res) => {
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/properties', propertyRouter);
+app.use('/api/v1/add', propertyRouter);
 
 const startServer = async () => {
     try {
